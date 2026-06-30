10e96a346ec4c282cebe7c9a12a8a63cbdd3bdd4
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-06-30 12:14:40 -0300

    feat/ botao funcional que adiciona tarefas

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 "use strict";
+class Tarefa {
+    titulo;
+    descricao;
+    constructor(tituloTarefa, descricaoTarefa) {
+        this.titulo = tituloTarefa;
+        this.descricao = descricaoTarefa;
+    }
+    criaTarefa() {
+        const li = document.createElement('li');
+        li.innerHTML = `<strong>${this.titulo}</strong>`;
+        if (this.descricao != undefined) {
+            li.innerHTML += ` (${this.descricao})`;
+        }
+        return li;
+    }
+}
+const btn = document.getElementById('addBtn');
+btn.addEventListener('click', () => {
+    const inputT = document.getElementById('tituloInput');
+    const inputD = document.getElementById('descricaoInput');
+    let novaTarefa = new Tarefa("", ""); //se for conts ela n muda
+    if (inputT.value === "")
+        return;
+    if (inputD.value === undefined) {
+        novaTarefa = new Tarefa(inputT.value);
+    }
+    else {
+        novaTarefa = new Tarefa(inputT.value, inputD.value);
+    }
+    document.getElementById('listaTarefas')?.appendChild(novaTarefa.criaTarefa());
+});
--- app.ts
+++ app.ts
@@ -2,23 +2,34 @@ class Tarefa{
     titulo: string;
     descricao?: string;
 
-    constructor(tituloTarefa: string) {
+    constructor(tituloTarefa: string, descricaoTarefa?:string) {
         this.titulo = tituloTarefa;
+        this.descricao = descricaoTarefa;
     }
 
     criaTarefa() {
         const li = document.createElement('li');
-        li.innerHTML=`${this.titulo}`
+        li.innerHTML=`<strong>${this.titulo}</strong>`
+        if (this.descricao != undefined){
+            li.innerHTML += ` (${this.descricao})`
+        }
         return li;
     }
 }
 
-/*
+
 const btn = document.getElementById('addBtn') as HTMLButtonElement;
 btn.addEventListener('click', () => {
-    const input = document.getElementById('tituloInput') as HTMLInputElement;
+    const inputT = document.getElementById('tituloInput') as HTMLInputElement;
+    const inputD = document.getElementById('descricaoInput') as HTMLInputElement;
+    let novaTarefa = new Tarefa("", ""); //se for conts ela n muda
+    if (inputT.value === "" ) return; 
+    if (inputD.value === undefined){ 
+        novaTarefa = new Tarefa(inputT.value);
+    }
+    else {
+        novaTarefa = new Tarefa(inputT.value, inputD.value);
+    }
 
-
-
+    document.getElementById('listaTarefas')?.appendChild(novaTarefa.criaTarefa());
 })
-*/

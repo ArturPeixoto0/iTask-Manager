eac9f5d8b72d48bb9eacc4b883d98adedb0be4e2
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-07-02 17:53:45 -0300

    feat/botao de deletar funcional e inicio do botao de completar

--- app.js
+++ app.js
@@ -1,64 +1,126 @@
 "use strict";
+function RemoveDaMemoria() {
+    for (let i = 0; i < Tarefas.length; i++) {
+        if (Tarefas[i].deletada === true) {
+            Tarefas.splice(i, 1);
+            i--;
+        }
+    }
+    localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+    for (let i = 0; i < Concluidas.length; i++) {
+        if (Concluidas[i].deletada === true) {
+            Concluidas.splice(i, 1);
+            i--;
+        }
+    }
+    localStorage.setItem("lista_concluidas", JSON.stringify(Concluidas));
+}
 class Tarefa {
     titulo;
     horario;
     completa;
+    deletada;
     descricao;
-    constructor(tituloTarefa, horarioCriacao = new Date, tarefaCompleta, descricaoTarefa) {
+    constructor(tituloTarefa, horarioCriacao = new Date, descricaoTarefa) {
         this.titulo = tituloTarefa;
         this.horario = horarioCriacao;
-        this.completa = tarefaCompleta;
+        this.completa = false;
+        this.deletada = false;
         this.descricao = descricaoTarefa;
+    }
+    completar() {
+        this.completa = true;
+    }
+    deletar() {
+        this.deletada = true;
     }
     criaTarefa() {
         const li = document.createElement('li');
         li.innerHTML = `<strong>${this.titulo}</strong>`;
-        if (this.descricao != undefined) {
-            li.innerHTML += ` (${this.descricao})`;
-        }
         li.innerHTML += ` | ${this.horario}  `;
+        if (this.descricao != undefined) {
+            li.innerHTML += `<br>(${this.descricao})<br>`;
+        }
         const botaoCOMPL = document.createElement('button');
         botaoCOMPL.innerHTML += "completar";
         botaoCOMPL.addEventListener('click', () => {
+            this.completar();
+            this.criaConcluida();
+            li.style.display = "none";
+            RemoveDaMemoria();
         });
         li.appendChild(botaoCOMPL);
         li.innerHTML += `  `;
         const botaoDEL = document.createElement('button');
         botaoDEL.innerHTML += "deletar";
         botaoDEL.addEventListener('click', () => {
+            li.style.display = "none";
+            this.deletar();
+            RemoveDaMemoria();
         });
         li.appendChild(botaoDEL);
         return li;
     }
-    deletar() {
-        this.titulo = "";
+    criaConcluida() {
+        const li = document.createElement('li');
+        li.innerHTML = `<strong>${this.titulo}</strong>`;
+        li.innerHTML += ` | ${this.horario}  `;
+        if (this.descricao != undefined) {
+            li.innerHTML += `<br>(${this.descricao})<br>`;
+        }
+        const botaoDEL = document.createElement('button');
+        botaoDEL.innerHTML += "deletar";
+        botaoDEL.addEventListener('click', () => {
+            li.style.display = "none";
+            this.deletar();
+            RemoveDaMemoria();
+        });
+        li.appendChild(botaoDEL);
+        return li;
     }
 }
 let Tarefas = [];
-const json = JSON.parse(localStorage.getItem("lista_tarefas") || "[]");
-for (let i = 0; i < json.length; i++) {
-    let aux = new Tarefa(json[i].titulo, new Date(json[i].horario), json[i].completa, json[i].descricao); //o aux é composto pelo primeiro elemento do json
-    Tarefas.push(aux);
+const jsonT = JSON.parse(localStorage.getItem("lista_tarefas") || "[]");
+for (let i = 0; i < jsonT.length; i++) {
+    let auxT = new Tarefa(jsonT[i].titulo, new Date(jsonT[i].horario), jsonT[i].descricao); //o aux é composto pelo primeiro elemento do json
+    Tarefas.push(auxT);
+}
+let Concluidas = [];
+const jsonC = JSON.parse(localStorage.getItem("lista_concluidas") || "[]");
+for (let i = 0; i < jsonC.length; i++) {
+    let auxC = new Tarefa(jsonC[i].titulo, new Date(jsonC[i].horario), jsonC[i].descricao); //o aux é composto pelo primeiro elemento do json
+    Tarefas.push(auxC);
 }
 for (let i = 0; i < Tarefas.length; i++) {
-    document.getElementById('listaTarefas')?.appendChild(Tarefas[i].criaTarefa());
+    if (Tarefas[i].completa === false) {
+        document.getElementById('listaTarefas')?.appendChild(Tarefas[i].criaTarefa());
+    }
+    else {
+        document.getElementById('listaConcluidas')?.appendChild(Tarefas[i].criaConcluida());
+    }
 }
 const btnAdicionar = document.getElementById('addBtn');
 btnAdicionar.addEventListener('click', () => {
     const inputT = document.getElementById('tituloInput');
     const inputD = document.getElementById('descricaoInput');
     const agora = new Date;
-    let novaTarefa = new Tarefa("", agora, false); //se for consts ela n muda
     if (inputT.value === "")
         return;
+    let novaTarefa = new Tarefa("", agora); //se for consts ela nao muda
     if (inputD.value === "") {
-        novaTarefa = new Tarefa(inputT.value, agora, false);
+        novaTarefa = new Tarefa(inputT.value, agora);
     }
     else {
-        novaTarefa = new Tarefa(inputT.value, agora, false, inputD.value);
+        novaTarefa = new Tarefa(inputT.value, agora, inputD.value);
     }
-    document.getElementById('listaTarefas')?.appendChild(novaTarefa.criaTarefa());
-    Tarefas.push(novaTarefa);
-    localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+    if (novaTarefa.deletada === false && novaTarefa.completa === false) {
+        document.getElementById('listaTarefas')?.appendChild(novaTarefa.criaTarefa());
+        Tarefas.push(novaTarefa);
+    }
+    if (novaTarefa.deletada === false && novaTarefa.completa === true) {
+        document.getElementById('listaConcluidas')?.appendChild(novaTarefa.criaConcluida());
+        Concluidas.push(novaTarefa);
+    }
 });
-//localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+localStorage.setItem("lista_concluidas", JSON.stringify(Concluidas));
--- app.ts
+++ app.ts
@@ -1,14 +1,42 @@
+function RemoveDaMemoria() {
+    for (let i =0; i<Tarefas.length; i++){
+        if (Tarefas[i].deletada === true){
+            Tarefas.splice(i, 1);
+            i--;
+        }
+    }
+    localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+
+    for (let i =0; i<Concluidas.length; i++){
+        if (Concluidas[i].deletada === true){
+            Concluidas.splice(i, 1);
+            i--;
+        }
+    }
+    localStorage.setItem("lista_concluidas", JSON.stringify(Concluidas));
+}
+
 class Tarefa{
     titulo: string;
     horario: Date;
     completa: boolean;
+    deletada: boolean;
     descricao?: string;
 
-    constructor(tituloTarefa: string, horarioCriacao:Date = new Date,tarefaCompleta: boolean, descricaoTarefa?:string) {
+    constructor(tituloTarefa: string, horarioCriacao:Date = new Date, descricaoTarefa?:string) {
         this.titulo = tituloTarefa;
         this.horario = horarioCriacao;
-        this.completa = tarefaCompleta;
+        this.completa = false;
+        this.deletada = false;
         this.descricao = descricaoTarefa;
+    }
+
+    completar(){
+        this.completa = true;
+    }
+
+    deletar (){
+        this.deletada = true;
     }
 
     criaTarefa() {
@@ -16,15 +44,20 @@ class Tarefa{
 
 
         li.innerHTML=`<strong>${this.titulo}</strong>`
+
+        li.innerHTML += ` | ${this.horario}  <br>`
+
         if (this.descricao != undefined){
-            li.innerHTML += ` (${this.descricao})`
+            li.innerHTML += `(${this.descricao})<br>`
         }
-        li.innerHTML += ` | ${this.horario}  `
 
         const botaoCOMPL = document.createElement('button');
         botaoCOMPL.innerHTML += "completar";
         botaoCOMPL.addEventListener('click', () => {
-            
+            this.completar();
+            this.criaConcluida();
+            li.style.display = "none"; 
+            RemoveDaMemoria();
         })
         li.appendChild(botaoCOMPL);
 
@@ -33,34 +66,76 @@ class Tarefa{
         const botaoDEL = document.createElement('button');
         botaoDEL.innerHTML += "deletar";
         botaoDEL.addEventListener('click', () => {
-            
+            li.style.display = "none"; 
+            this.deletar();
+            RemoveDaMemoria();
         })
         li.appendChild(botaoDEL);
+
 
         return li;
     }
 
-    deletar (){
-    this.titulo = "";
+    criaConcluida(){
+        const li = document.createElement('li');
+
+        li.innerHTML=`<strong>${this.titulo}</strong>`
+
+        li.innerHTML += ` | ${this.horario}  <br>`
+
+        if (this.descricao != undefined){
+            li.innerHTML += `(${this.descricao})<br>`
+        }
+
+        const botaoDEL = document.createElement('button');
+        botaoDEL.innerHTML += "deletar";
+        botaoDEL.addEventListener('click', () => {
+            li.style.display = "none"; 
+            this.deletar();
+            RemoveDaMemoria();
+        })
+        li.appendChild(botaoDEL);
+
+
+        return li;
+    
     }
 }
 
+
 let Tarefas: Tarefa[] = [];
-const json = JSON.parse(localStorage.getItem("lista_tarefas") || "[]");
+const jsonT = JSON.parse(localStorage.getItem("lista_tarefas") || "[]");
 
 
-for (let i = 0; i<json.length; i++){
-    let aux = new Tarefa(
-    json[i].titulo,
-    new Date(json[i].horario),
-    json[i].completa,
-    json[i].descricao,
+for (let i = 0; i<jsonT.length; i++){
+    let auxT = new Tarefa(
+    jsonT[i].titulo,
+    new Date(jsonT[i].horario),
+    jsonT[i].descricao,
     ) //o aux é composto pelo primeiro elemento do json
-    Tarefas.push(aux);
+    Tarefas.push(auxT);
+}
+
+let Concluidas: Tarefa[] = [];
+const jsonC = JSON.parse(localStorage.getItem("lista_concluidas") || "[]");
+
+
+for (let i = 0; i<jsonC.length; i++){
+    let auxC = new Tarefa(
+    jsonC[i].titulo,
+    new Date(jsonC[i].horario),
+    jsonC[i].descricao,
+    ) //o aux é composto pelo primeiro elemento do json
+    Tarefas.push(auxC);
 }
 
 for(let i = 0; i<Tarefas.length; i++){
-    document.getElementById('listaTarefas')?.appendChild(Tarefas[i].criaTarefa());
+    if (Tarefas[i].completa === false){
+        document.getElementById('listaTarefas')?.appendChild(Tarefas[i].criaTarefa());
+    }
+    else {
+        document.getElementById('listaConcluidas')?.appendChild(Tarefas[i].criaConcluida());
+    }
 }
 
 
@@ -69,22 +144,34 @@ btnAdicionar.addEventListener('click', () => {
     const inputT = document.getElementById('tituloInput') as HTMLInputElement;
     const inputD = document.getElementById('descricaoInput') as HTMLInputElement;
     const agora: Date = new Date;
-    let novaTarefa = new Tarefa("", agora, false); //se for consts ela n muda
+
     if (inputT.value === "" ) return; 
+    
+    let novaTarefa = new Tarefa("", agora); //se for consts ela nao muda
+    
     if (inputD.value === ""){ 
-        novaTarefa = new Tarefa(inputT.value, agora, false);
+        novaTarefa = new Tarefa(inputT.value, agora);
     }
     else {
-        novaTarefa = new Tarefa(inputT.value, agora, false, inputD.value);
+        novaTarefa = new Tarefa(inputT.value, agora, inputD.value);
     }
-    document.getElementById('listaTarefas')?.appendChild(novaTarefa.criaTarefa());
 
-    Tarefas.push(novaTarefa);
-    localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+    if (novaTarefa.deletada === false && novaTarefa.completa === false){
+        document.getElementById('listaTarefas')?.appendChild(novaTarefa.criaTarefa());
+        Tarefas.push(novaTarefa);
+    }
+    if (novaTarefa.deletada === false && novaTarefa.completa === true){
+        document.getElementById('listaConcluidas')?.appendChild(novaTarefa.criaConcluida());
+        Concluidas.push(novaTarefa);
+    }
+
 })
 
 
-//localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+
+
+localStorage.setItem("lista_tarefas", JSON.stringify(Tarefas));
+localStorage.setItem("lista_concluidas", JSON.stringify(Concluidas));
 
 
 
